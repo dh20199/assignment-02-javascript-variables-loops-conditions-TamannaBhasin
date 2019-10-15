20b164c73f2e1eb845518c98c3720d5754da8ac9
Tamanna Bhasin <tamanna.bhasin@mail.utoronto.ca>
2019-10-15 17:40:08 -0400

    Changes to 01

--- Problems/01-functions-and-variables.js
+++ Problems/01-functions-and-variables.js
@@ -166,7 +166,7 @@ function typeTester (unknown) {
   } else if (typeof unknown === 'number') {
     return unknown * unknown ;
   } else if (typeof unknown === 'undefined') {
-    return 'Sorry, I can\'t do anything with a a undefined value.' ; 
+    return 'Sorry, I can\'t do anything with an undefined value.' ; 
   } else {
     return 'I don\'t know how to use that kind of variable.' ; 
   }
